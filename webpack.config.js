3947f54b466fd2f172f83f84040f65cb25659e67
Yuri Zotov <yzotov@phoenixit.ru>
2020-02-12 16:34:07 +0300

    css-modules-setup Check ts

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.(ts|tsx)?$/,
                 loader: "awesome-typescript-loader"
             },
             {
