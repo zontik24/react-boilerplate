d77c354571fa2149e3f3dc94ceb895cb794c4e27
Yuri Zotov <yzotov@phoenixit.ru>
2020-02-13 11:36:21 +0300

    Fix typescript loader regex extension

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(ts|tsx)?$/,
+                test: /\.(ts|tsx)$/,
                 loader: "awesome-typescript-loader"
             },
             {
